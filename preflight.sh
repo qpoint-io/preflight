b6457c850dbfa53fad0fcc0dd32d1d668d871424
Jon Friesen <jon@jonfriesen.ca>
2024-10-30 17:26:50 -0600

    remove example URL

--- preflight.sh
+++ preflight.sh
@@ -191,8 +191,7 @@ check_root() {
             "FAIL" \
             "non-root" \
             "root" \
-            "This script must be run with root privileges. Please run with: sudo $0" \
-            "https://docs.example.com/root-requirements"
+            "This script must be run with root privileges. Please run with: sudo $0"
         critical_failure_exit
     fi
 }
